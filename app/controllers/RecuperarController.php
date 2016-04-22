95457b33cf7b1c8bf99ea1efd283b2f9a2835b33
Bruno Santos <contato@hxtutors.com.br>
2016-04-22 15:31:53 -0300

    Add action sair

--- app/controllers/RecuperarController.php
+++ app/controllers/RecuperarController.php
@@ -2,5 +2,17 @@
 
 class RecuperarController extends \HXPHP\System\Controller
 {
-	
+	public function __construct($configs)
+	{
+		parent::__construct($configs);
+
+		$this->load(
+			'Services\Auth',
+			$configs->auth->after_login,
+			$configs->auth->after_logout,
+			true
+		);
+
+		$this->auth->redirectCheck(true);
+	}
 }
