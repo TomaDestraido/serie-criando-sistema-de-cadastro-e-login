4ca55b649186e2acf81658cf43bb2060e01d20ea
Bruno Santos <contato@hxtutors.com.br>
2016-04-22 15:14:01 -0300

    Configurações de produção

--- app/config.php
+++ app/config.php
@@ -15,4 +15,17 @@
 
 	$configs->env->development->auth->setURLs('/sistema/home/', '/sistema/login/');
 
+	$configs->env->add('production');
+
+	$configs->env->production->baseURI = '/';
+
+	$configs->env->production->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user' => 'hxphp190_sistema',
+		'password' => '^EKI1LK(&kV[',
+		'dbname' => 'hxphp190_sistemahx'
+	));
+
+	$configs->env->production->auth->setURLs('/home/', '/login/');
+
 	return $configs;
