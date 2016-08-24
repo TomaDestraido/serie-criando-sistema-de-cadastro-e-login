3420fd351a7cffca0ad0a1eb7cb98e9184dfa5d1
Bruno Santos <contato@hxtutors.com.br>
2016-08-24 17:00:16 -0300

    Configuração do menu para administrador

--- app/config.php
+++ app/config.php
@@ -17,9 +17,16 @@
 
 	$configs->env->development->menu->setMenus(array(
 		'Home/dashboard' => '%baseURI%/home',
-		'Projetos/briefcase' => '%baseURI%/projetos',
 		'Editar perfil/cog' => '%baseURI%/perfil/editar',
+		'Sair/exit' => '%baseURI%/login/sair',
 	), 'user');
+
+	$configs->env->development->menu->setMenus(array(
+		'Home/dashboard' => '%baseURI%/home',
+		'Usuários/users' => '%baseURI%/usuarios',
+		'Editar perfil/cog' => '%baseURI%/perfil/editar',
+		'Sair/exit' => '%baseURI%/login/sair'
+	), 'administrator');
 
 	$configs->env->development->menu->setMenus(array(
 		'Home/dashboard' => '%baseURI%/home'
@@ -46,9 +53,16 @@
 
 	$configs->env->production->menu->setMenus(array(
 		'Home/dashboard' => '%baseURI%/home',
-		'Projetos/briefcase' => '%baseURI%/projetos',
 		'Editar perfil/cog' => '%baseURI%/perfil/editar',
+		'Sair/exit' => '%baseURI%/login/sair',
 	), 'user');
+
+	$configs->env->production->menu->setMenus(array(
+		'Home/dashboard' => '%baseURI%/home',
+		'Usuários/users' => '%baseURI%/usuarios',
+		'Editar perfil/cog' => '%baseURI%/perfil/editar',
+		'Sair/exit' => '%baseURI%/login/sair'
+	), 'administrator');
 
 	$configs->env->production->menu->setMenus(array(
 		'Home/dashboard' => '%baseURI%/home'
